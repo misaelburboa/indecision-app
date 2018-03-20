e0bf9de229d813818e4ff4f4fac460cb2d875e03
Misael Burboa <caleb.burboa@internetbrands.com>
2018-03-20 07:59:58 -0700

    24 Classes Part 1

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,19 @@
+class Person {
+    constructor(name = 'Anonymous', age = 0) {
+        this.name   = name;
+        this.age    = age;
+    }
+    getGretting(){
+        //return 'Hi I am ' + this.name + '!';
+        return `Hi. I am ${this.name}!`
+    }
+    getDescription(){
+        return `${this.name} is ${this.age} year(s) old`;
+    }
+}
+
+const me = new Person('Misael Burboa', 29);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getGretting());
