26dbea286158982d8cfff4ab037e06b5d6bc6c37
Misael Burboa <caleb.burboa@internetbrands.com>
2018-03-26 09:12:48 -0700

    Class properties syntax

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map', //shows the error queue easily for developer, it is used to track the error
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
